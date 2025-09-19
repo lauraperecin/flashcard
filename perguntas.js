c0ea9051d65273da29aca1583640f9308a029086
lauraperecin <perecin.oliveira@escola.pr.gov.br>
2025-09-19 12:15:49 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,14 @@
 criaCartao(
-    ‘categoria’,
-    ‘pergunta’,
+    ‘categoria’,conhecimento gerais
+    ‘pergunta’,>Quem foi a primeira pessoa a viajar no espaço?</p>
+last month
+
+Create index.html
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+last month
+
+Update index.html
+                      <p>Yuri Gagarin.</p>
     ‘resposta’
 )
